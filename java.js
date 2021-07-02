31edb711ffa098181b077e27708e656488a3e3fb
Jason-Cee <jasoncee017@gmail.com>
2021-07-02 16:20:34 +0200

    Mobile responsiveness

--- java.js
+++ java.js
@@ -28,22 +28,19 @@ function getPokemonInfo(url) {
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
-      // Make sure data comes throufg
+      // Make sure data comes through
       console.log(data);
       fetch(data.species.url)
         .then((res) => res.json())
         .then((speciesData) => {
           console.log(speciesData);
           // Write data to pokemon information container
-          document.querySelector(".details").innerHTML = `
-    <p>${speciesData.flavor_text_entries[1].flavor_text}</p>
-    `;
         });
       document.querySelector(".pokemon-info").innerHTML = `<img src="${
         data.sprites.front_default
       } ">
       ${"Name: " + data.species.name} <br> ${"Height: " + data.height} <br> ${
         "Weight: " + data.weight + "kg's"
-      } <br> ${"Details:"}`;
+      }`;
     });
 }
